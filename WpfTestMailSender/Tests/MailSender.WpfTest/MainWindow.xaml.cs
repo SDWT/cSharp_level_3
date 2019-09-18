861e0f52200ec674dd0136d4836e21a9fd8a86b3
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-18 21:01:51 +0300

    Lesson1: Prepare

---
+++ WpfTestMailSender/Tests/MailSender.WpfTest/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+
+namespace MailSender.WpfTest
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            var host = "smtp.mail.ru";
+            var port = 25;
+
+            string msg = string.Empty;
+
+            var user_name = UserNameEditor.Text;
+
+            //var password = PasswordEditor.Password;
+            var password = PasswordEditor.SecurePassword;
+
+            using (var client = new SmtpClient(host, port))
+            {
+                client.EnableSsl = true;
+                client.Credentials = new NetworkCredential(user_name, password);
+                using (var message = new MailMessage())
+                {
+                    message.From = new MailAddress("Sender@mail.ru", "Дима");
+                    message.To.Add(new MailAddress("Sender@mail", "Дима"));
+                    //message.To.Add(new MailAddress("Sender@mail", "Дима"));
+                    message.Subject = "Заголовок письма от " + DateTime.Now;
+                    message.Body = msg;
+                    message.IsBodyHtml = false;
+
+                    try
+                    {
+                        client.Send(message);
+                        MessageBox.Show("Почта успешна отправлена", "Успех!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+        }
+    }
+}
--- WpfTestMailSender/Tests/WpfTestMailSender/WpfMailSender.xaml.cs
+++ WpfTestMailSender/Tests/WpfTestMailSender/WpfMailSender.xaml.cs
@@ -1,40 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 using System.Net;
 using System.Net.Mail;
+using System.Security;
 
-namespace WinFormsTestMailSender
+namespace WpfTestMailSender
 {
-    public partial class Form1 : Form
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class WpfMailSender : Window
     {
-        public Form1()
+        public WpfMailSender()
         {
             InitializeComponent();
-
-            // 
-            ToolTip tltSender = new ToolTip();
-            tltSender.SetToolTip(tbxSender, "Отправитель");
-
-            ToolTip tltPassword = new ToolTip();
-            tltPassword.SetToolTip(passwordBox, "Пароль");
         }
 
-        private void BtnSendEmail_Click(object sender, EventArgs e)
+        private void BtnSendEmail_Click(object sender, RoutedEventArgs e)
         {
-            // Emails list 
             List<string> listStrMails = new List<string>();
             listStrMails.Add(tbxSender.Text);
 
             string strSender = tbxSender.Text;
-            string strPassword = passwordBox.Text;
+            var password = passwordBox.SecurePassword;
 
             foreach (var mail in listStrMails)
             {
@@ -43,23 +44,25 @@ namespace WinFormsTestMailSender
                     mm.Subject = "Hello from C#";    // Email title / Тема
                     mm.Body = "Hello world! hahaha"; // Email body / Текст
                     mm.IsBodyHtml = false;           // false - body hasnt html
-
+            
                     using (var sc = new SmtpClient("smtp.mail.ru", 25))
                     {
                         sc.EnableSsl = true;
-                        sc.Credentials = new NetworkCredential(strSender, strPassword);
+                        sc.Credentials = new NetworkCredential(strSender, password);
                         try
                         {
                             sc.Send(mm);
                         }
                         catch (Exception ex)
                         {
-                            MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
                         }
                     } // smtp
                 } // mm
             }
-            MessageBox.Show("Работа завершена.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            SendEndWindow sew = new SendEndWindow();
+            sew.ShowDialog();
         }
     }
 }
