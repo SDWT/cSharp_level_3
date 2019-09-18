7956e5c91cfbffcfbed2e9e5eb9890031b3cc1fe
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-18 15:42:33 +0300

    Add SendEndWindow

--- WpfTestMailSender/WpfTestMailSender/WpfMailSender.xaml.cs
+++ WpfTestMailSender/WpfTestMailSender/WpfMailSender.xaml.cs
@@ -37,31 +37,32 @@ namespace WpfTestMailSender
             string strSender = tbxSender.Text;
             string strPassword = passwordBox.Password;
 
-            foreach (var mail in listStrMails)
-            {
-                using (var mm = new MailMessage(strSender, mail))
-                {
-                    mm.Subject = "Hello from C#";    // Email title / Тема
-                    mm.Body = "Hello world! hahaha"; // Email body / Текст
-                    mm.IsBodyHtml = false;           // false - body hasnt html
-
-                    using (var sc = new SmtpClient("smtp.mail.ru", 25))
-                    {
-                        sc.EnableSsl = true;
-                        sc.Credentials = new NetworkCredential(strSender, strPassword);
-                        try
-                        {
-                            sc.Send(mm);
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                        }
-                    } // smtp
-                } // mm
-            }
-            MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
-
+            //foreach (var mail in listStrMails)
+            //{
+            //    using (var mm = new MailMessage(strSender, mail))
+            //    {
+            //        mm.Subject = "Hello from C#";    // Email title / Тема
+            //        mm.Body = "Hello world! hahaha"; // Email body / Текст
+            //        mm.IsBodyHtml = false;           // false - body hasnt html
+            //
+            //        using (var sc = new SmtpClient("smtp.mail.ru", 25))
+            //        {
+            //            sc.EnableSsl = true;
+            //            sc.Credentials = new NetworkCredential(strSender, strPassword);
+            //            try
+            //            {
+            //                sc.Send(mm);
+            //            }
+            //            catch (Exception ex)
+            //            {
+            //                MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            //            }
+            //        } // smtp
+            //    } // mm
+            //}
+            //MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            SendEndWindow sew = new SendEndWindow();
+            sew.ShowDialog();
         }
     }
 }
