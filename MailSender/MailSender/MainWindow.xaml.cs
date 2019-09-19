65614f28df0b23f8ab07927ce69981094585d60f
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-19 13:54:50 +0300

    Transfer from WpfTestMailSender.WpfTestMailSender

--- MailSender/MailSender/MainWindow.xaml.cs
+++ MailSender/MailSender/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
+using System.Security;
 
 namespace MailSender
 {
@@ -23,6 +15,42 @@ namespace MailSender
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> listStrMails = new List<string>();
+            listStrMails.Add(tbxSender.Text);
+
+            string strSender = tbxSender.Text;
+            var password = passwordBox.SecurePassword;
+
+            foreach (var mail in listStrMails)
+            {
+                using (var mm = new MailMessage(strSender, mail))
+                {
+                    mm.Subject = "Hello from C#";    // Email title / Тема
+                    mm.Body = "Hello world! hahaha"; // Email body / Текст
+                    mm.IsBodyHtml = false;           // false - body hasnt html
+
+                    using (var sc = new SmtpClient("smtp.mail.ru", 25))
+                    {
+                        sc.EnableSsl = true;
+                        sc.Credentials = new NetworkCredential(strSender, password);
+                        try
+                        {
+                            sc.Send(mm);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    } // smtp
+                } // mm
+            }
+            //MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+            SendEndWindow sew = new SendEndWindow();
+            sew.ShowDialog();
         }
     }
 }
