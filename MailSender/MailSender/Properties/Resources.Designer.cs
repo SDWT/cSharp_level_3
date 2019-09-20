de9e09b80126cb7ba04406451049ccce4b2c781b
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-20 16:06:14 +0300

    Add margin to Email Body and Email Title textBox Add new icons to tabControl

--- MailSender/MailSender/Properties/Resources.Designer.cs
+++ MailSender/MailSender/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace MailSender.Properties {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Doc {
+            get {
+                object obj = ResourceManager.GetObject("Doc", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Edit {
             get {
                 object obj = ResourceManager.GetObject("Edit", resourceCulture);
