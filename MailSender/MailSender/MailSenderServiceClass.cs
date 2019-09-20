b37f94988f68c3b8b2988facb6ddc2355515a278
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-20 21:06:35 +0300

    HW 1b MailSenderServiceClass

---
+++ MailSender/MailSender/MailSenderServiceClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Mail;
+using System.Security;
+
+namespace MailSender
+{
+    class MailSenderServiceClass
+    {
+        public MailSenderServiceClass()
+        {
+
+        }
+
+        /// <summary>
+        /// Send email message by smtp without html
+        /// </summary>
+        /// <param name="sender">Sender email</param>
+        /// <param name="password">Sender password</param>
+        /// <param name="smtpUrl">smtp server url</param>
+        /// <param name="smtpPort">smtp server port</param>
+        /// <param name="title">Email title</param>
+        /// <param name="body">Email main part</param>
+        /// <param name="destination">Giver email</param>
+        /// <returns>false if message don't send</returns>
+        public bool SmtpSendMessage(string sender, SecureString password,
+            string smtpUrl, int smtpPort,
+            string title, string body,
+            string destination)
+        {
+            var listStrEmails = new List<string>();
+            listStrEmails.Add(destination);
+
+            var tries = SmtpSendMessages(sender, password,
+                smtpUrl, smtpPort, title, body, listStrEmails);
+            if (tries.Count == 1)
+                return tries[0];
+            throw new ArgumentException("Destination not added or create a lot of emails.");
+        }
+
+        /// <summary>
+        /// Send email message by smtp without html
+        /// </summary>
+        /// <param name="sender">Sender email</param>
+        /// <param name="password">Sender password</param>
+        /// <param name="smtpUrl">smtp server url</param>
+        /// <param name="smtpPort">smtp server port</param>
+        /// <param name="title">Email title</param>
+        /// <param name="body">Email main part</param>
+        /// <param name="listStrEmails">Giver emails list</param>
+        /// <returns>list of try sending results, false if message don't send</returns>
+        public List<bool> SmtpSendMessages(string sender, SecureString password, 
+            string smtpUrl, int smtpPort,
+            string title, string body, 
+            List<string> listStrEmails)
+        {
+            List<bool> tries = new List<bool>();
+
+            foreach (var mail in listStrEmails)
+            {
+                using (var mm = new MailMessage(sender, mail))
+                {
+                    mm.Subject = title;    // Email title / Тема
+                    mm.Body = body;        // Email body / Текст
+                    mm.IsBodyHtml = false; // false - body hasnt html
+
+                    using (var sc = new SmtpClient(smtpUrl, smtpPort))
+                    {
+                        sc.EnableSsl = true;
+                        sc.Credentials = new NetworkCredential(sender, password);
+                        try
+                        {
+                            sc.Send(mm);
+                            tries.Add(true);
+                        }
+                        catch (Exception ex)
+                        {
+                            tries.Add(false);
+                        }
+                    } // smtp
+                } // mm
+            }
+            return tries;
+        }
+    }
+}
--- MailSender/MailSender/WpfMailSender.xaml.cs
+++ MailSender/MailSender/WpfMailSender.xaml.cs
@@ -19,38 +19,74 @@ namespace MailSender
             InitializeComponent();
         }
 
+        //private void BtnSendEmail_Click(object sender, RoutedEventArgs e)
+        //{
+        //    List<string> listStrMails = new List<string>();
+        //    listStrMails.Add(tbxSender.Text);
+
+        //    string strSender = tbxSender.Text;
+        //    var password = passwordBox.SecurePassword;
+
+        //    foreach (var mail in listStrMails)
+        //    {
+        //        using (var mm = new MailMessage(strSender, mail))
+        //        {
+        //            mm.Subject = txtBxEmailTitle.Text;    // Email title / Тема
+        //            mm.Body = txtBxEmailBody.Text; // Email body / Текст
+        //            mm.IsBodyHtml = false;           // false - body hasnt html
+
+        //            using (var sc = new SmtpClient(Smtps.MailRu.SmptUrl, Smtps.MailRu.Port))
+        //            {
+        //                sc.EnableSsl = true;
+        //                sc.Credentials = new NetworkCredential(strSender, password);
+        //                try
+        //                {
+        //                    sc.Send(mm);
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        //                }
+        //            } // smtp
+        //        } // mm
+        //    }
+        //    //MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+        //    SendEndWindow sew = new SendEndWindow();
+        //    sew.ShowDialog();
+        //}
+
         private void BtnSendEmail_Click(object sender, RoutedEventArgs e)
         {
+            var SendService = new MailSenderServiceClass();
+
+            // Givers
             List<string> listStrMails = new List<string>();
             listStrMails.Add(tbxSender.Text);
 
+            // Sender
             string strSender = tbxSender.Text;
             var password = passwordBox.SecurePassword;
 
-            foreach (var mail in listStrMails)
-            {
-                using (var mm = new MailMessage(strSender, mail))
-                {
-                    mm.Subject = txtBxEmailTitle.Text;    // Email title / Тема
-                    mm.Body = txtBxEmailBody.Text; // Email body / Текст
-                    mm.IsBodyHtml = false;           // false - body hasnt html
+            // Email
+            string title = txtBxEmailTitle.Text;    // Email title / Тема
+            string body = txtBxEmailBody.Text; // Email body / Текст
 
-                    using (var sc = new SmtpClient(Smtps.MailRu.SmptUrl, Smtps.MailRu.Port))
-                    {
-                        sc.EnableSsl = true;
-                        sc.Credentials = new NetworkCredential(strSender, password);
-                        try
-                        {
-                            sc.Send(mm);
-                        }
-                        catch (Exception ex)
-                        {
-                            MessageBox.Show($"Ошибка при отправке {ex.ToString()}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                        }
-                    } // smtp
-                } // mm
+            var results = SendService.SmtpSendMessages(strSender, password,
+                Smtps.MailRu.SmptUrl, Smtps.MailRu.Port, title, body, listStrMails);
+            for (int i = 0; i < results.Count; i++)
+            {
+                if (results[i])
+                {
+                    MessageBox.Show($"Message to {listStrMails[i]} sended.", 
+                        "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Ошибка письмо до {listStrMails[i]} не отправлено.",
+                        "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
-            //MessageBox.Show("Работа завершена.", "Notification", MessageBoxButton.OK, MessageBoxImage.Information);
+
             SendEndWindow sew = new SendEndWindow();
             sew.ShowDialog();
         }
