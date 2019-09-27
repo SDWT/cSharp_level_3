c297094f75016435ade57176485531633c6e0daa
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-27 12:40:51 +0300

    ListController from lesson3

---
+++ MailSender/Tests/MailSender.WPFTest/MailSenderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MailSender.lib.MVVM;
+
+namespace MailSender.WPFTest 
+{
+    class MailSenderViewModel : ViewModel
+    {
+        private string _Title = "Заголовок";
+    }
+}
