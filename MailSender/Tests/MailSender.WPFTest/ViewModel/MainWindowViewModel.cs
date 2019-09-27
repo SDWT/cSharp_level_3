4e7b27e690be43540d9e68cc7cb1f25ca51d8675
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-09-27 16:05:34 +0300

    Add RecipientEditor

--- MailSender/Tests/MailSender.WPFTest/ViewModel/MainWindowViewModel.cs
+++ MailSender/Tests/MailSender.WPFTest/ViewModel/MainWindowViewModel.cs
@@ -4,7 +4,9 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using MailSender.lib.Data.Linq2SQL;
 using MailSender.lib.Services;
 
@@ -23,10 +25,30 @@ namespace MailSender.WPFTest.ViewModel
 
         public ObservableCollection<Recipient> Recipients { get; } = new ObservableCollection<Recipient>();
 
+        public ICommand RefreshDataCommand { get; }
+
         public MainWindowViewModel(RecipientsDataProvider RecipientsProvider)
         {
             _RecipientsProvider = RecipientsProvider;
 
+            RefreshDataCommand = new RelayCommand(OnRefreshDataCommandExecuted, CanRefreshDataCommandExecute);
+            //RefreshData();
+        }
+
+        #region SelectedRecipient
+        private Recipient _SelectedRecipient;
+        public Recipient SelectedRecipient 
+        {
+            get => _SelectedRecipient;
+            set => Set(ref _SelectedRecipient, value);
+        }
+
+        #endregion
+
+        private bool CanRefreshDataCommandExecute() => true;
+
+        private void OnRefreshDataCommandExecuted()
+        {
             RefreshData();
         }
 
