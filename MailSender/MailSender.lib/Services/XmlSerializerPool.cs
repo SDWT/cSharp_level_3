b34450f6126d14939179c85474c67e465e1cc2aa
Дмитрий Самсонов <NomekopSD@yandex.ru>
2019-10-04 12:18:53 +0300

    XmlSerializerPool ConcurrentDictionary usage example

---
+++ MailSender/MailSender.lib/Services/XmlSerializerPool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MailSender.lib.Services
+{
+    public static class XmlSerializerPool
+    {
+        private static readonly ConcurrentDictionary<Type, XmlSerializer> _SerializerPool = new ConcurrentDictionary<Type, XmlSerializer>();
+
+        public static XmlSerializer GetXmlSerializer<T>() => GetXmlSerializer(typeof(T));
+
+        public static XmlSerializer GetXmlSerializer(Type ObjectType)
+        {
+            return _SerializerPool.GetOrAdd(ObjectType, type => new XmlSerializer(type));
+
+        }
+    }
+}
